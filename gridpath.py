2165a8ce8158caf28fffe6c6d1baa4e675ccaa5e
admaga <adrian.mtz.ga@gmail.com>
2022-10-21 17:25:32 -0600

    Updated comments on code.

--- gridpath.py
+++ gridpath.py
@@ -9,166 +9,110 @@ NON_OBSTACLE_CHARACTER = 0
 class World(object):
     def __init__(self, world, start, end):
         self.world = world
-        self.start = start
-        # Set the end character in the world
-        self.world[start[0]][start[1]] = START_CHARACTER
-        self.end = end
-        # Set the end character in the world
-        self.world[end[0]][end[1]] = END_CHARACTER
         self.rows = len(world)
         self.cols = len(world[0])
-        self.painted = False
+
+        self.start = start
+        self.world[start[0]][start[1]] = START_CHARACTER
+
+        self.end = end
+        self.world[end[0]][end[1]] = END_CHARACTER
      
-    def getNeighbors(self, current):
-        currentRow, currentCol = current
+    def getNeighbors(self, cell):
+        currentRow, currentColumn = cell
         # Possible direction of agent: up, down, right, left
         directions = [(-1,0), (1,0), (0,1), (0,-1)]
-        for direction in directions:
-            # Apply movement to current position
-            neighborRow, neighborCol = currentRow + direction[0], currentCol + direction[1]
+        for rowIncrement, columnIncrement in directions:
+            futureCellRow, futureCellColumn = currentRow + rowIncrement, currentColumn + columnIncrement
             # If the neighbor is out of the world or if there is an obstacle
-            if (neighborRow < 0 or neighborRow >= self.rows or neighborCol < 0 or neighborCol >= self.cols or self.world[neighborRow][neighborCol] == OBSTACLE_CHARACTER):
+            if ((futureCellRow < 0 or futureCellRow >= self.rows or futureCellColumn < 0 or futureCellColumn >= self.cols) or self.world[futureCellRow][futureCellColumn] == OBSTACLE_CHARACTER):
                 continue
-            yield (neighborRow, neighborCol)
+            yield (futureCellRow, futureCellColumn)
     
     def reachedGoal(self, cell):
         return self.world[cell[0]][cell[1]] == 9
 
-    def backtrack(self, current, movements):
+    def backtrack(self, cell, movements):
         path = []
-        while current != self.start:
-            path.append(current)
-            current = movements[current]
+        while cell != self.start:
+            path.append(cell)
+            cell = movements[cell]
         path.reverse()
         return path[:-1]
 
     def g(self):
-        # Cost of one step is 1, not diagonal movement
+        # Cost of one step is 1, no diagonal movement
         return 1
 
-    def h(self, currentCell):
-        # Heuristic of manhattan distance on square grid
-        return abs(currentCell[0] - self.end[0]) + abs(currentCell[1] - self.end[1])
+    def h(self, cell):
+        # Heuristic of Manhattan's distance
+        return abs(cell[0] - self.end[0]) + abs(cell[1] - self.end[1])
         
     def breadthFirstSearch(self):
-        # Create frontier
         frontier = []
         frontier.append(self.start)
-
-        # Create path tracker
         movements = {}
         movements[self.start] = None
-        
         while (len(frontier) != 0):
             currentCell = frontier.pop()
-
             if self.reachedGoal(currentCell):
-                # Backtrack and print path
                 return self.backtrack(currentCell, movements)
-
-            # Iterate over each possible neighbor
             for neighbor in self.getNeighbors(currentCell):
-                # Add the neighbor that we have not seen to the frontier
                 if neighbor not in movements:
-                    # Add breadcrumb in dictionary key:toCell value:fromCell
                     movements[neighbor] = currentCell
                     frontier.append(neighbor)
     
     def uniformCostSearch(self):
-        # Create frontier
         frontier = PriorityQueue()
-        # Priority by g(n)
-        frontier.put((0, self.start))
-
-        # Create path tracker, KEY=toCell VALUE=fromCell
+        frontier.put((0, self.start))    # Priority by g(n)
         movements = {}
         movements[self.start] = None
-
-        # Create cost tracker, KEY=celL VALUE=cost of reaching this cell
-        cellCost = {}
+        cellCost = {}    # Cost tracker, cost of reaching this cell
         cellCost[self.start] = 0
-        
         while (not frontier.empty()):
             frontierCell = frontier.get()
             currentCell = frontierCell[1]
-            
             if self.reachedGoal(currentCell):
-                # Backtrack and print path
                 return self.backtrack(currentCell, movements)
-
-            # Iterate over each possible neighbor
             for neighbor in self.getNeighbors(currentCell):
-                # Calculate the cost of move
                 g = cellCost[currentCell] + self.g()
-                # Expand the node with lowest path cost
-                if neighbor not in movements or g < cellCost[neighbor]:
-                    # Add breadcrumb in dictionary
+                if neighbor not in movements or g < cellCost[neighbor]:    # Expand the neighbor with lowest path cost
                     movements[neighbor] = currentCell
-                    # Add the neighbor to sort by cost
                     frontier.put((g, neighbor))
                     cellCost[neighbor] = g
     
     def greedyBestFirstSearch(self):
-        # Create frontier
         frontier = PriorityQueue()
-        # Priority by h(n)
-        frontier.put((0, self.start))
-
-        # Create path tracker, KEY=toCell VALUE=fromCell
+        frontier.put((0, self.start))    # Priority by h(n)
         movements = {}
         movements[self.start] = None
-        
         while (not frontier.empty()):
             frontierCell = frontier.get()
             currentCell = frontierCell[1]
-            
             if self.reachedGoal(currentCell):
-                # Backtrack and print path
                 return self.backtrack(currentCell, movements)
-
-            # Iterate over each possible neighbor
             for neighbor in self.getNeighbors(currentCell):
-                # Calculate the cost of move
-                h = self.h(currentCell)
-                # Expand the node not seen 
                 if neighbor not in movements:
-                    # Add breadcrumb in dictionary
                     movements[neighbor] = currentCell
-                    # Add the neighbor to sort by heuristic
-                    frontier.put((h, neighbor))
+                    frontier.put((self.h(currentCell), neighbor))
     
     def aStarSearch(self):
-        # Create frontier
         frontier = PriorityQueue()
-        # Priority by f(n) = g(n) + h(n)
-        frontier.put((0, self.start))
-
-        # Create path tracker, KEY=toCell VALUE=fromCell
+        frontier.put((0, self.start))    # Priority by f(n) = g(n) + h(n)
         movements = {}
         movements[self.start] = None
-
-        # Create cost tracker, KEY=celL VALUE=cost of reaching this cell
-        cellCost = {}
+        cellCost = {}    # Cost tracker, cost of reaching this cell
         cellCost[self.start] = 0
-        
         while (not frontier.empty()):
             frontierCell = frontier.get()
             currentCell = frontierCell[1]
-            
             if self.reachedGoal(currentCell):
-                # Backtrack and print path
                 return self.backtrack(currentCell, movements)
-
-            # Iterate over each possible neighbor
             for neighbor in self.getNeighbors(currentCell):
-                # Calculate the cost of move
                 g = cellCost[currentCell] + self.g()
-                # Expand the node with lowest path cost
                 if neighbor not in movements or g < cellCost[neighbor]:
-                    # Add breadcrumb in dictionary
                     movements[neighbor] = currentCell
                     f = g + self.h(currentCell)
-                    # Add the neighbor to sort by cost
                     frontier.put((f, neighbor))
                     cellCost[neighbor] = g
                 
@@ -178,17 +122,3 @@ class World(object):
                 print(self.world[x][y], end="")
             print()
         print()
-
-# world = [[0,0,0,0,1],
-#          [0,1,1,0,1],
-#          [0,0,0,0,1],
-#          [0,1,0,1,1],
-#          [0,0,0,0,9]]
-
-# myWorld = World(world)
-# myWorld.printWorld()
-
-# myWorld.breadthFirstSearch()
-# myWorld.uniformCostSearch()
-# myWorld.greedyBestFirstSearch()
-# myWorld.aStarSearch()
--- gridpath_gui.py
+++ gridpath_gui.py
@@ -10,18 +10,12 @@ NUM_ROWS = 20
 START_POSITION = (0,0)
 END_POSITION = (NUM_COLS - 1, NUM_ROWS  -1)
 
-# Create grid data structure
 gridButtons = [[0 for i in range(NUM_COLS)] for j in range(NUM_ROWS)]
-
-# Create the world object
 myWorld = gridpath.World(copy.deepcopy(gridButtons), START_POSITION, END_POSITION)
-myWorld.printWorld()
 
-# Functions of buttons pressed
 def paintCell(cell):
     gridButtons[cell[0]][cell[1]]["bg"] = "yellow"
 
-# Paint the world with the search path
 def paintSearchAgorithm(pathList):
     if not pathList:
         messagebox.showinfo(message="No solution with current obstacles.", title="Alert")
@@ -45,7 +39,6 @@ def aStarButtonPressed():
 def resetWorldButtonPressed():
     for i in range(NUM_ROWS):
         for j in range(NUM_COLS):
-            # If cell is START or END
             if i == START_POSITION[0] and j == START_POSITION[1]:
                 bgColor = "red"
                 myWorld.world[i][j] = START_CHARACTER
@@ -57,7 +50,7 @@ def resetWorldButtonPressed():
             else:
                 bgColor = "white"
                 myWorld.world[i][j] = NON_OBSTACLE_CHARACTER
-            # Change background color
+
             gridButtons[i][j]["bg"] = bgColor
 
 def clearPathButtonPressed():
@@ -67,7 +60,6 @@ def clearPathButtonPressed():
             if currentBG == "black":
                 continue
             bgColor = "white"
-            # If cell is START or END
             if i == myWorld.start[0] and j == myWorld.start[1]:
                 bgColor = "red"
             if i == myWorld.end[0] and j == myWorld.end[1]:
@@ -77,19 +69,14 @@ def clearPathButtonPressed():
 def randomizeObstaclesButtonPressed():
     for i in range(NUM_ROWS):
         for j in range(NUM_COLS):
-            # Dont change START and END
             if (i == myWorld.start[0] and j == myWorld.start[1]) or (i == myWorld.end[0] and j == myWorld.end[1]):
                 continue
-
             currentBG = gridButtons[i][j]["bg"]
-            # Reset the path cell
             if currentBG == "yellow":
                 gridButtons[i][j]["bg"] = "white"
                 myWorld.world[i][j] = NON_OBSTACLE_CHARACTER
                 continue
-            
-            # Randomly toggle the obstacle 
-            if random.randint(0,5) == 1:
+            if random.randint(0,5) == 1:    # Randomly toggle the obstacle 
                 if currentBG == "black":
                     gridButtons[i][j]["bg"] = "white"
                     myWorld.world[i][j] = NON_OBSTACLE_CHARACTER
@@ -101,7 +88,7 @@ def randomizeObstaclesButtonPressed():
 mainWindow = tk.Tk()
 mainWindow.title("GRID WORLD SEARCH ALGORITHMS")
 
-# Frame for search buttons
+# Set frame for buttons
 frameButtons = tk.Frame(mainWindow,relief=tk.RAISED, bd=2)
 breadthFirstSearchButton = tk.Button(frameButtons, text="Breadth First Search", command=lambda:breadthFirstSearchButtonPressed())
 uniformCostSearchButton = tk.Button(frameButtons, text="Uniform Cost Search", command=lambda:uniformCostSearchButtonPressed())
@@ -111,7 +98,7 @@ randomizeObstaclesButton = tk.Button(frameButtons, text="Randomize Obstacles", c
 clearPathButton = tk.Button(frameButtons, text="Clear Path", command=lambda:clearPathButtonPressed())
 resetWorldButton = tk.Button(frameButtons, text="Reset World", command=lambda:resetWorldButtonPressed())
 
-# Set the position of the buttons
+# Set position of the buttons
 breadthFirstSearchButton.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 uniformCostSearchButton.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
 greedySearchButton.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
@@ -125,49 +112,40 @@ frameButtons.grid(row=0, column=0, sticky="ns")
 frameWorld=tk.Frame(mainWindow)
 frameWorld.grid(row=0, column=1, sticky="nsew")
 
-# Function when a button is pressed
 def buttonPressed(i, j):
     currentBG = gridButtons[i][j]["bg"]
     if myWorld.start == None:
-        # Set this button to be the START and update world
         newBG = "red"
         myWorld.start = (i, j)
         myWorld.world[i][j] = START_CHARACTER
     elif myWorld.end == None:
-        # Set this button to be the END and update world
         newBG = "blue"
         myWorld.end = (i, j)
         myWorld.world[i][j] = END_CHARACTER
     elif currentBG == "white":
-        # Toggle background color
         newBG = "black"
         myWorld.world[i][j] = OBSTACLE_CHARACTER
     elif currentBG == "black":
-        # Toggle background color
         newBG = "white"
         myWorld.world[i][j] = NON_OBSTACLE_CHARACTER
     elif currentBG == "red":
-        # Flag the red button and reset current to white
         newBG = "white"
         myWorld.start = None
         myWorld.world[i][j] = NON_OBSTACLE_CHARACTER
     elif currentBG == "blue":
-        # Flag the blue button and reset current to white
         newBG = "white"
         myWorld.end = None
         myWorld.world[i][j] = NON_OBSTACLE_CHARACTER
     else:
-        # Toggle background color for other cases
         newBG = "black"
         myWorld.world[i][j] = OBSTACLE_CHARACTER
-    # Change the background color
+
     gridButtons[i][j]["bg"] = newBG
 
-# Create all the button objects and save them in data structure
+# Create button objects
 for i in range(NUM_COLS):
     for j in range(NUM_ROWS):
         bgColor = "white"
-        # If cell is START or END
         if i == START_POSITION[0] and j == START_POSITION[1]:
             bgColor = "red"
         if i == END_POSITION[0] and j == END_POSITION[1]:
